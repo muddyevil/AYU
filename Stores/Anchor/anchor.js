3ffcdf27d2d746c847808395ca74613af3e0321c
Meriadoc Nathaniel <muddyevil@hotmail.com>
2022-04-22 15:01:42 +0100

    added menu toggles

---
+++ Stores/Anchor/anchor.js
@@ -0,0 +1,35 @@
+function toggleBreakfast() {
+    var x = document.getElementById("breakfastMenu");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function toggleLunch() {
+    var x = document.getElementById("lunchMenu");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function toggleDinner() {
+    var x = document.getElementById("dinnerMenu");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function toggleDrinks() {
+    var x = document.getElementById("drinksMenu");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
